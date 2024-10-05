3daeccf0852339dc4b6e588595c9e06b70db514c
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-04 19:38:05 -0700

    allocate buffers on GPUs

--- matmul.py
+++ matmul.py
@@ -1,12 +1,18 @@
 import torch
 import torch.distributed as dist
 
+dist.init_process_group(backend='nccl')
+
+my_rank = dist.get_rank()
+world_size = dist.get_world_size()
+torch.cuda.set_device(my_rank % torch.cuda.device_count())
+my_device = torch.cuda.current_device()
+
+print("my_rank " + str(my_rank) + "/" + str(world_size) + " my_device " + str(my_device) + "/" + str(torch.cuda.device_count()) + "\n")
+
 A = torch.empty(3, 4)
-
-if torch.cuda.is_available():
-    print('We have a GPU!')
-else:
-    print('Sorry, CPU only.')
-
-print("Hello\n")
+B = torch.empty(3, 4, device=my_device)
+C = A.to(my_device)
 print(A)
+print(B)
+print(C)
