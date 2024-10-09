8fedfa9b910ec91d3ba01037437533b371e2acf4
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-08 20:40:06 -0700

    implement 2D TP (works only for power of two)

--- matmul.py
+++ matmul.py
@@ -209,7 +209,8 @@ def matmul_rowwise(hidden_dim = 16384, batch_size = 1024, num_layers = 118, TP =
                 print("row-wise layer %d" % (layer), end=" ")
                 FLOPs = 2 * A.size(0) * A.size(1) * B_buff.size(1)
                 Bytes = B_buff.element_size() * B_buff.nelement()
-                print("matmul %.2f (%.2f TFLOPS) comm %.2f (%.2f GB/s) matmul+comm = %.2f overhead %.2f us" % (matmul*1e3, FLOPs / (matmul / 1e3) / 1e12, comm*1e3, Bytes / (comm / 1e3) / 1e9, (matmul+comm)*1e3, total*1e6-(matmul+comm)*1e3), end=" ")   
+
+                print("comm %.2f (%.2f GB/s) matmul %.2f (%.2f TFLOPS) comm+matmul = %.2f overhead %.2f us" % (comm*1e3, Bytes / (comm / 1e3) / 1e9, matmul*1e3, FLOPs / (matmul / 1e3) / 1e12, (comm+matmul)*1e3, total*1e6-(comm+matmul)*1e3), end=" ")
                 print("total %.2f max %.2f us" % (total * 1e6, max_ * 1e6))
     return B
 
