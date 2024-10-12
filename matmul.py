24618c2bf06aa07ac674b0888439385dcf6306e5
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-11 18:12:55 -0700

    implement global view of P2P communications

--- matmul.py
+++ matmul.py
@@ -306,7 +306,7 @@ def matmul_2D(hidden_dim = 16384, batch_size = 1024, num_layers = 126, TP=8, DP 
                 if local_rank == recver:
                     dist.recv(recvbuf, sender, group=group_TP)
         torch.cuda.synchronize()
-        dist.barrier()
+        # dist.barrier()
         dist.all_gather_into_tensor(testbuff, recvbuf, group=group_TP)
         torch.cuda.synchronize()
         dist.barrier()
