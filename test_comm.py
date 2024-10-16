74caeba5cd9d713bb09569e821d77847143f82fa
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-15 21:45:16 -0700

    implement deepspeed library

--- test_comm.py
+++ test_comm.py
@@ -12,8 +12,10 @@ torch.cuda.set_device(my_rank % torch.cuda.device_count())
 my_device = torch.cuda.current_device()
 root_rank = 7
 
-
+# Load the seventh column from the file "log_0.txt"
+data = np.loadtxt('log_0.txt', usecols=6)
 
 if my_rank == root_rank:
-    print("my_rank " + str(my_rank) + "/" + str(world_size) + " my_device " + str(my_device) + "/" + str(torch.cuda.device_count()) + "\n")
+    print("my_rank " + str(my_rank) + "/" + str(world_size) + " my_device " + str(my_device) + "/" + str(torch.cuda.device_count()))
+    print(data)
  
