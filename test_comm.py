5760f5a075d0910a369aee2cf9e393fadd7516d4
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-15 22:12:46 -0700

    implement deepspeed library

--- test_comm.py
+++ test_comm.py
@@ -11,6 +11,11 @@ torch.cuda.set_device(my_rank % torch.cuda.device_count())
 my_device = torch.cuda.current_device()
 root_rank = 7
 
+def find_max(time):
+    time_max = torch.tensor([time], device=my_device)
+    torch.distributed.all_reduce(time_max, op=torch.distributed.ReduceOp.MAX)
+    return time_max.item()
+
 # import number of elements
 data = np.loadtxt('log_0.txt', usecols=4, dtype=int)
 print(data)
@@ -39,10 +44,9 @@ for i in range(0, 3000):
     time_end = time.perf_counter()
     event_time = event_start.elapsed_time(event_end)
     perf_time = time_end - time_start
-    time_max = torch.tensor([perf_time], device=my_device)
-    torch.distributed.all_reduce(time_max, op=torch.distributed.ReduceOp.MAX)
-    time_max = time_max.item()
+    event_max = find_max(event_time)
+    time_max = find_max(perf_time)
     if my_rank == root_rank:
-        print(f"perf {perf_time*1e6:.2f} event {event_time*1e3:.2f} ms throughput: {bytes / event_time / 1e9:.2f} GB/s")
+        print(f"perf {perf_time*1e6:.2f} event {event_time*1e3:.2f} perf max {time_max*1e6:.2f} event max {event_max*1e3:.2f} ms throughput: {bytes / event_time / 1e9:.2f} GB/s")
 
  
