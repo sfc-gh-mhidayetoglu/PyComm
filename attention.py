45257d0fe7d9c1313e92d2773145c79c0afdca89
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-23 13:46:29 -0700

    implement Ulysses attention

--- attention.py
+++ attention.py
@@ -113,9 +113,9 @@ def ulysses(seq_length, hidden_dim, num_heads, P) -> torch.Tensor:
         print(f"c [h/P, N, d/h]: {c.shape}, elements: {c.nelement()}, size {c.element_size() * c.nelement() / 1e6:.2f} MB")
         torch.cuda.synchronize()
         print(f"Peak memory allocation: {torch.cuda.max_memory_allocated() / 1e9:.2f} GB")
-    # c = torch.reshape(c.transpose(0, 1), (seq_length, num_heads//P, hidden_dim//num_heads))
+    c = torch.reshape(c.permute(1, 0, 2), (seq_length, num_heads//P, hidden_dim//num_heads))
     # c.transpose(0, 1)
-    c.permute(1, 0, 2)
+    # c.permute(1, 0, 2)
     if my_rank == root_rank:
         print("transpose c")
         print(f"c [N, h/P, d/h]: {c.shape}, elements: {c.nelement()}, size {c.element_size() * c.nelement() / 1e6:.2f} MB")
