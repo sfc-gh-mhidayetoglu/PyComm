953d37d151a430300d312632779e7d1308560f97
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-31 21:25:23 -0700

    implement transformer

--- transformer.py
+++ transformer.py
@@ -450,7 +450,7 @@ my_device = torch.cuda.current_device()
 root_rank = 7
 
 # model parameters
-seq_length = 40000  # N
+seq_length = 30000  # N
 hidden_dim = 16384  # d
 num_heads = 128     # h
 inter_size = 53248  # d'
