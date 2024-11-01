aa99a30eea1c33a1a85cd7156e34bded8174318b
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-10-31 23:28:20 -0700

    implement transformer

--- transformer.py
+++ transformer.py
@@ -538,7 +538,7 @@ def attention_2D(input, Q, K, V, O, q, k, v, c, q_, k_, v_, c_, attention, group
     q = torch.matmul(input, Q)
     k = torch.matmul(input, K)
     v = torch.matmul(input, V)
-    q.transpose_(0, 1)
+    q.transpose_(0, 1).contiguous()
     if my_rank == root_rank:
         print(f"q shape: {q.shape}")
         print(f"q_ shape: {q_.shape}")
