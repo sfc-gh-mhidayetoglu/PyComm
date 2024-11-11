0a4b8963190e4046a1fea696d923827de6bc231d
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-11 13:56:08 -0800

    replay msccl++

--- test_comm.py
+++ test_comm.py
@@ -48,32 +48,34 @@ if my_rank == root_rank:
     print(data)
     print(count)
     print(f"Buffer size in MB: {bytes / 1e6}")
-exit()
 
-log_new = open('log.txt', 'w')
-event_start = torch.cuda.Event(enable_timing=True)
-event_end = torch.cuda.Event(enable_timing=True)
-for i in range(0, 3000):
+# log_new = open('log.txt', 'w')
+# event_start = torch.cuda.Event(enable_timing=True)
+# event_end = torch.cuda.Event(enable_timing=True)
+
+for i in range(data):
     buff_ = buff.narrow(0, 0, data[i])
-    bytes = buff_.numel() * buff_.element_size()
-    torch.cuda.synchronize()
-    dist.barrier()
-    time_start = time.perf_counter()
-    event_start.record()
+    # bytes = buff_.numel() * buff_.element_size()
+    # torch.cuda.synchronize()
+    # dist.barrier()
+    # time_start = time.perf_counter()
+    # event_start.record()
     if custom == True:
         buff_, _ = comm.all_reduce(buff_)
     else:
         dist.all_reduce(buff_)
-    event_end.record()
-    torch.cuda.synchronize()
-    time_end = time.perf_counter()
-    event_time = event_start.elapsed_time(event_end)
-    perf_time = time_end - time_start
-    event_max = find_max(event_time)
-    time_max = find_max(perf_time)
-    if my_rank == root_rank:
-        log_new.write(f"{i} {data[i]} {perf_time*1e6:.2f} {event_time*1e3:.2f} {time_max*1e6:.2f} {event_max*1e3:.2f}\n")
+    # event_end.record()
+    # torch.cuda.synchronize()
+    # time_end = time.perf_counter()
+    # event_time = event_start.elapsed_time(event_end)
+    # perf_time = time_end - time_start
+    # event_max = find_max(event_time)
+    # time_max = find_max(perf_time)
+    # if my_rank == root_rank:
+    #     log_new.write(f"{i} {data[i]} {perf_time*1e6:.2f} {event_time*1e3:.2f} {time_max*1e6:.2f} {event_max*1e3:.2f}\n")
     # if my_rank == root_rank:
     #     print(f"iter {i} {data[i]} elements perf {perf_time*1e6:.2f} event {event_time*1e3:.2f} perf max {time_max*1e6:.2f} event max {event_max*1e3:.2f} us throughput: {bytes / event_time / 1e6:.2f} GB/s")
+    if my_rank == root_rank:
+        print(f"iter {i} {data[i]} elements")
 
 dist.destroy_process_group()
